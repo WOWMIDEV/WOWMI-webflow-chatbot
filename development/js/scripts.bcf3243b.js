d21654baf19032f8279da751925baa7a1ec5ea68
Victor Kasap <kasap.victor@gmail.com>
2023-02-06 13:27:09 +0200

    feat WATERMARK #8941260: Added new chat

--- development/js/scripts.bcf3243b.js
+++ development/js/scripts.bcf3243b.js
@@ -2413,7 +2413,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var chatTemplate = function chatTemplate(name) {
-  return "<div class=\"chatbot__title\">\n              <span class=\"chatbot__name\">".concat(name, "</span>\n              <span class=\"chatbot__reset\">\u21BA</span>\n              <span class=\"chatbot__close\">&times;</span>\n          </div>\n          <div class=\"chatbot__wrapper\">\n            <div class=\"chatbot__items\"></div>\n          </div>\n          <div class=\"chatbot__footer\">\n            <input class=\"chatbot__input\" type=\"text\" disabled placeholder=\"Reply to Gold Financial\">\n            <button class=\"chatbot__submit\" type=\"button\" disabled></button>\n          </div>");
+  return "<div class=\"chatbot__title\">\n              <span class=\"chatbot__name\">".concat(name, "</span>\n              <span class=\"chatbot__reset\">\u21BA</span>\n              <span class=\"chatbot__close\">&times;</span>\n          </div>\n          <div class=\"chatbot__wrapper\">\n            <div class=\"chatbot__items\"></div>\n          </div>\n          <div class=\"chatbot__footer\">\n            <input class=\"chatbot__input\" type=\"text\" disabled placeholder=\"Reply to Watermark\">\n            <button class=\"chatbot__submit\" type=\"button\" disabled></button>\n          </div>");
 };
 var _default = chatTemplate;
 exports.default = _default;
