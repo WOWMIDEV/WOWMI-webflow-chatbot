5b9a2202331cd313f4165cc2cb281dafeb891a7c
Victor Kasap <kasap.victor@gmail.com>
2023-02-20 10:06:59 +0200

    feat PRMG ORIGINATOR #8850202: Added new chat

--- development/js/styles.5c8f3eab.js
+++ development/js/styles.5c8f3eab.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/logo.png":[["logo.cecc779a.png","img/logo.png"],"img/logo.png"],"./../svg/loader2.svg":[["loader2.c23a5573.svg","svg/loader2.svg"],"svg/loader2.svg"],"./../svg/close.svg":[["close.b35eae0d.svg","svg/close.svg"],"svg/close.svg"],"./../svg/send.svg":[["send.e0767347.svg","svg/send.svg"],"svg/send.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../svg/logo.svg":[["logo.ff614f16.svg","svg/logo.svg"],"svg/logo.svg"],"./../svg/loader2.svg":[["loader2.c23a5573.svg","svg/loader2.svg"],"svg/loader2.svg"],"./../svg/close.svg":[["close.b35eae0d.svg","svg/close.svg"],"svg/close.svg"],"./../svg/send.svg":[["send.e0767347.svg","svg/send.svg"],"svg/send.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55432" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57759" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
