e365ea02ae1f09ef4e549d959e6a25ceb1f40f09
Victor Kasap <kasap.victor@gmail.com>
2023-02-20 10:01:27 +0200

    feat PRMG ORIGINATOR #8850202: Added new chat

--- src/scripts/templates/chat.js
+++ src/scripts/templates/chat.js
@@ -7,7 +7,7 @@ const chatTemplate = (name) => `<div class="chatbot__title">
             <div class="chatbot__items"></div>
           </div>
           <div class="chatbot__footer">
-            <input class="chatbot__input" type="text" disabled placeholder="Reply to Advantage Mortgage">
+            <input class="chatbot__input" type="text" disabled placeholder="Reply to PRMG">
             <button class="chatbot__submit" type="button" disabled></button>
           </div>`;
 
